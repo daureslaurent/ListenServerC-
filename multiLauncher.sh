9a2648b12cbf853c026d6e8f7af3aedeb6d94f76
laurent Daures <laurent.daures@epitech.eu>
2018-08-05 01:19:20 +0200

    fix

--- multiLauncher.sh
+++ multiLauncher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
- ./serv 2121 rec &> alloutput.txt &
- ./serv 2122 rec &> alloutput.txt &
- ./serv 2123 rec &> alloutput.txt &
- ./serv 2124 rec &> alloutput.txt &
- ./serv 2110 rec &> alloutput.txt &
+ ./serv 2121 rec > alloutput.txt &
+ ./serv 2122 rec > alloutput.txt &
+ ./serv 2123 rec > alloutput.txt &
+ ./serv 2124 rec > alloutput.txt &
+ ./serv 2110 rec > alloutput.txt &
