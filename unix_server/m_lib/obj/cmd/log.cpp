fe5ee7f990c07437e9f50d2479f770685019f80d
laurent Daures <laurent.daures@epitech.eu>
2018-08-14 16:26:37 +0200

    testSh

--- unix_server/m_lib/obj/cmd/log.cpp
+++ unix_server/m_lib/obj/cmd/log.cpp
@@ -22,7 +22,7 @@ std::string	CMD_log::Run()
 {
   std::string path = "out"+std::to_string(_port)+"/log_connection.log";
   std::cout << "path[" << path << "]" << std::endl;
-  std::ifstream		fichier("out"+std::to_string(_port)+"/log_connection.log", std::ios::in);  // on ouvre en lecture
+  std::ifstream		fichier(path, std::ios::in);  // on ouvre en lecture
   std::string		final;
 
   if (fichier)  // si l'ouverture a fonctionné
--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 git pull
+cd unix_server/
 time make serveur
+mv serv ../serv
