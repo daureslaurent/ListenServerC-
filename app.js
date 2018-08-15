f36402668fe33b07f740102a5c7f0efbabf06d32
laurent Daures <laurent.daures@epitech.eu>
2018-08-15 14:53:46 +0200

    fix

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ net.createServer(function (socket) {
   // Handle incoming messages from clients.
   socket.on('data', function (data) {
     //Check multiple msg
-    var listMsg = data.split("}");
+    var listMsg = data.toString().split("}");
     //loop on msg
     for (let index = 0; index < listMsg.length; index++) {
       const msg = listMsg[index];
