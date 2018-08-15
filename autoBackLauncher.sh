b107a973967c20a22d5105e1ec62f2b13086e5ef
laurent Daures <laurent.daures@epitech.eu>
2018-08-15 05:23:07 +0200

    fix path

--- autoBackLauncher.sh
+++ autoBackLauncher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$1" != "" ]; then
-    mkdir ../out$1
-    mkdir ../out$1/logServer
+    mkdir ./out$1
+    mkdir ./out$1/logServer
     echo "Launch ListenServer on :$1"
     ./serv $1 recorder > ./out$1/logServer/outLog-$1.txt
 else
