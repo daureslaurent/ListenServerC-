88eae4fd347cbac90b271fc9f26de46cbd9001d1
laurent Daures <laurent.daures@epitech.eu>
2018-08-15 18:17:53 +0200

    fix

--- update.sh
+++ update.sh
@@ -3,14 +3,14 @@ git checkout autoBackLauncher.sh
 git pull
 chmod +x autoBackLauncher.sh
 chmod +x launcher.sh
-if [ -f "serv" ]
+if [ -f "serv.bin" ]
 then
 	echo "No need compile server ..."
 else
 	echo "Need compil ..."
     cd unix_server/
     time make serveur
-    mv serv ../serv
+    mv serv ../serv.bin
 fi
 
 echo "Update finshed !"
