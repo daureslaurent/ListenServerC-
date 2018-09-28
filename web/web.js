d13c64cf9deb60eb9c3ac149480e0dadda1da891
laurent Daures <laurent.daures@epitech.eu>
2018-09-28 22:09:19 +0200

    fix errType

--- web/web.js
+++ web/web.js
@@ -63,50 +63,52 @@ module.exports = function(app) {
 
     app.get('/web/server', function(req, res){
         var timeStampStart = new Date().getTime();
-        dataConverter.recurciveUnixTest(function(servers){
-            if (req.query.addr){
-                var addr = req.query.addr;
-                var name = req.query.name;
-                var port = req.query.port;
-                var redirect = req.query.redirect;
-                console.log('CreateServer['+addr+']['+name+']['+port+']['+redirect+']');
-                serverCtrl.createServer(addr, name, port, redirect);
-                dataCtrl.createData(data);
-                res.render('server', { serverList: servers });
-            }
-            else if (req.query.todel){
-                var id = req.query.todel;
-                serverCtrl.deleteServer(id);
-                res.render('server', { serverList: servers });
-            }
-            else if (req.query.serverId){
-                var serverId = req.query.serverId;
-                serverCtrl.getServerByIdCb(serverId, function(dataNAN){
-                    var data = dataNAN[0];
-                    dataConverter.getDataByPortLimitCallBack(data.port, 5, function(dataList){
-                        serverCmd.getLogUnixServer(data.port, function(logUnix){
-                            utils.testUnixServerCb(data.ip, data.port, function(state){
-                                if (state){
-                                    utils.getLogServerUnix(data.ip, data.port, function(log){
-                                        utils.getVersionServerUnix(data.ip, data.port, function(version){
-                                            var gentTime =((new Date().getTime())-timeStampStart);
-                                            res.render('serverControl', { dataList: dataList, serverData: data, serverState: state, serverLog: log, unixLog: logUnix, serverVersion: version, genTime: gentTime });
-                                        })
-                                    });
-                                }
-                                else {
-                                    var gentTime =((new Date().getTime())-timeStampStart);
-                                    res.render('serverControl', { dataList: dataList, serverData: data, serverState: state, serverLog: 'NO CONNECTION', unixLog: logUnix, serverVersion: 'undefined', genTime: gentTime });
-                                }
+        if (req.query.addr){
+            var addr = req.query.addr;
+            var name = req.query.name;
+            var port = req.query.port;
+            var redirect = req.query.redirect;
+            console.log('CreateServer['+addr+']['+name+']['+port+']['+redirect+']');
+            serverCtrl.createServer(addr, name, port, redirect);
+            res.send('OK');
+        }
+        else {
+            dataConverter.recurciveUnixTest(function(servers){
+                if (req.query.todel){
+                    var id = req.query.todel;
+                    serverCtrl.deleteServer(id);
+                    res.render('server', { serverList: servers });
+                }
+                else if (req.query.serverId){
+                    var serverId = req.query.serverId;
+                    serverCtrl.getServerByIdCb(serverId, function(dataNAN){
+                        var data = dataNAN[0];
+                        dataConverter.getDataByPortLimitCallBack(data.port, 5, function(dataList){
+                            serverCmd.getLogUnixServer(data.port, function(logUnix){
+                                utils.testUnixServerCb(data.ip, data.port, function(state){
+                                    if (state){
+                                        utils.getLogServerUnix(data.ip, data.port, function(log){
+                                            utils.getVersionServerUnix(data.ip, data.port, function(version){
+                                                var gentTime =((new Date().getTime())-timeStampStart);
+                                                res.render('serverControl', { dataList: dataList, serverData: data, serverState: state, serverLog: log, unixLog: logUnix, serverVersion: version, genTime: gentTime });
+                                            })
+                                        });
+                                    }
+                                    else {
+                                        var gentTime =((new Date().getTime())-timeStampStart);
+                                        res.render('serverControl', { dataList: dataList, serverData: data, serverState: state, serverLog: 'NO CONNECTION', unixLog: logUnix, serverVersion: 'undefined', genTime: gentTime });
+                                    }
+                                });
                             });
                         });
                     });
-                });
-            }
-            else {
-                res.render('server', { serverList: servers });
-            }
-        });
+                }
+                else {
+                    res.render('server', { serverList: servers });
+                }
+            });
+        }
+        
     });
 
     app.get('/web/search', function(req, res){
