9cc7e85b33a82f91e6df90c5d6a2b10864058053
laurent Daures <laurent.daures@epitech.eu>
2018-09-29 15:28:41 +0200

    clean

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 var config = {
     apiPort: process.env.PORT || 3000,
-    finalDB: 'mongodb://192.168.1.17/listenServerV2'
+    finalDB: 'mongodb://192.168.1.17/listenServerReborn'
 };
 module.exports = config;
